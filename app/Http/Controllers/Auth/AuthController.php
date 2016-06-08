faa96d10564cfe3bc857ec028af64f9287c6f191
Nikola Ljumovic <nikola.ljumovic@gmail.com>
2016-06-08 13:38:39 +0200

    small fix

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use Validator;
 use App\Http\Controllers\Controller;
