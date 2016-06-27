76d572d1245908f743d704c63d86bba6931635ef
Nikola Ljumovic <nikola.ljumovic@gmail.com>
2016-06-27 20:38:56 +0200

    Nav Bar changed

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -3,12 +3,14 @@
         <div class="navbar-header">
 
             <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+            <div class="hamburger">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
@@ -32,7 +34,8 @@
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
+                            <img class="nav-profile-photo m-r-xs" src="{{ Auth::user()->photo_url }}">
+                            <span class="caret"></span>
                         </a>
 
                         <ul class="dropdown-menu" role="menu">
